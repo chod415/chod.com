f19f776fe649ba1c1717194314bb0cb36c3cdaf2
chod415 <chodeahyeon76@gmail.com>
2025-11-04 11:13:19 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,13 @@ module.exports = function(eleventyConfig) {
 
   // --- CSS 파일 복사 ---
   eleventyConfig.addPassthroughCopy("style.css");
-
+  
   // --- 관리자 페이지 폴더 복사 ---
   // (이것이 '하얀 화면' 문제의 해결책입니다!)
   // admin 폴더 안의 모든 파일(index.html, config.yml)을
   // _site 폴더로 그대로 복사합니다.
   eleventyConfig.addPassthroughCopy("admin");
-
+  
   // --- 다른 HTML 페이지들 복사 ---
   eleventyConfig.addPassthroughCopy("게임유튜브.html");
   eleventyConfig.addPassthroughCopy("헬스유튜브.html");
